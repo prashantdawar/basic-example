4193f4d22048df7ba44ffd6cbe83ee97addb4ce2
prashantdawar <prashantdawar.2008@gmail.com>
2018-08-12 19:47:41 +0530

    ch6

--- controllers/ReservationsController.php
+++ controllers/ReservationsController.php
@@ -5,6 +5,7 @@ namespace app\controllers;
 use Yii;
 use yii\web\Controller;
 use app\models\Reservation;
+use app\models\Room;
 use app\models\ReservationSearch;
 use yii\data\ActiveDataProvider;
 
@@ -51,4 +52,89 @@ class ReservationsController extends Controller {
             'resultQueryAveragePricePerDay' => $resultQueryAveragePricePerDay
         ]);
     }
+
+    /**
+     * Action for displaying reservationa and rooms grid in same page
+     * completely independent from each other
+     */
+
+     public function actionMultipleGrid(){
+
+        /**
+         * Reservations
+         */
+
+         $reservationsQuery = Reservation::find();
+         $reservationsSearchModel = new ReservationSearch();
+
+         if(isset($_GET['ReservationSearch'])) {
+
+            $reservationsSearchModel->load(\Yii::$app->request->get());
+
+            $reservationsQuery->joinWith(['customer']);
+            $reservationsQuery->andFilterWhere([
+                'LIKE',
+                'customer.surname',
+                $reservationsSearchModel->getAttribute('customer.surname')
+            ]);
+
+            $reservationsQuery->andFilterWhere([
+                'id' => $reservationsSearchModel->id,
+                'customer_id' => $reservationsSearchModel->customer_id,
+                'room_id' => $reservationsSearchModel->room_id,
+                'price_per_day' => $reservationsSearchModel->price_per_day
+            ]);
+         }
+
+         $reservationsDataProvider = new ActiveDataProvider([
+                'query' => $reservationsQuery,
+                'sort' => [
+                    'sortParam' => 'reservations-sort-param'
+                ],
+                'pagination' => [
+                    'pageSize' => 10,
+                    'pageParam' => 'reservations-page-param'
+                ]
+         ]);
+
+         /**
+          * Rooms
+          */
+
+          $roomsQuery = Room::find();
+          $roomsSearchModel = new Room();
+
+          if(isset($_GET['Room'])){
+
+            $roomsSearchModel->load(\Yii::$app->request->get());
+
+            $roomsQuery->andFilterWhere([
+                'id' => $roomsSearchModel->id,
+                'floor' => $roomsSearchModel->floor,
+                'room_number' => $roomsSearchModel->room_number,
+                'has_conditioner' => $roomsSearchModel->has_conditioner,
+                'has_phone' => $roomsSearchModel->has_phone,
+                'has_tv' => $roomsSearchModel->has_tv,
+                'available_from' => $roomsSearchModel->available_from
+            ]);
+          }
+
+          $roomsDataProvider = new ActiveDataPRovider([
+                'query' => $roomsQuery,
+                'sort' => [
+                    'sortParam' => 'rooms-sort-param'
+                ],
+                'pagination' => [
+                    'pageSize' => 10,
+                    'pageParam' => 'rooms-page-param'
+                ]
+          ]);
+
+          return $this->render('multipleGrid', [
+            'reservationsDataProvider' => $reservationsDataProvider,
+            'reservationsSearchModel' => $reservationsSearchModel,
+            'roomsDataProvider' => $roomsDataProvider,
+            'roomsSearchModel' => $roomsSearchModel
+          ]);
+     }
 }
